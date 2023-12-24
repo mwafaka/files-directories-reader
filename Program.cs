228d8e9d17686cab851876e31319b92895e3f413
mwafaka <mwafak.arbash@digitalcareerinstitute.org>
2023-12-24 14:01:45 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+var currentDirectory = Directory.GetCurrentDirectory();
+var datasDirectory = Path.Combine(currentDirectory, "data");
+
+var newDirectory = Path.Combine(currentDirectory, "newDirectory");
+Directory.CreateDirectory(newDirectory);
+
+var salesFiles = FindFiles(datasDirectory);
+
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+File.AppendAllText(Path.Combine(newDirectory, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+Console.WriteLine(CalculateSalesTotal(salesFiles));
+
+record SalesData(double Total);
